86855d449e1aae358e3dd626a9ec7839b11f30bc
xzorbixltd <46635368+xzorbixltd@users.noreply.github.com>
2019-01-12 23:17:26 +0000

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,39 @@
+# Hopefully this will configure the servers initially
+# Lets start by disabling the Network Manager & Restarting the network
+
+systemctl stop NetworkManager.service
+systemctl disable NetworkManager.service
+systemctl enable network.service
+systemctl start network.service
+
+# Now we can get ready to install cPanel
+
+yum -y install nano
+yum -y install perl
+
+# Now we can install cPanel
+
+cd /home
+curl -o latest -L https://securedownloads.cpanel.net/latest
+sh latest
+
+# Now we can install CSF
+
+cd /usr/src
+rm -fv csf.tgz
+wget https://download.configserver.com/csf.tgz
+tar -xzf csf.tgz
+cd csf
+sh install.sh
+
+# Now we can install Softaculous
+cd /home
+wget -N http://files.softaculous.com/install.sh
+chmod 755 install.sh
+./install.sh
+
+# Now I can delete this script
+
+echo "Well nothing blew up, that's a good start!"
+
+rm -- "$0"
